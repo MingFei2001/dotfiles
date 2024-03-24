8c01f1a499875fe835ee6ee44a2159de0276037a
MingFei2001 <mingfei2001@gmail.com>
2024-03-24 22:26:41 +0800

    added lazygit plugins

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -62,6 +62,8 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- Exiting nvim
+vim.keymap.set('n', '<leader>q', '<cmd>q<CR>', { desc = 'Exit Neovim' })
 -- | -----------------------------
 
 -- | -----------------------------
