7bcb9985b89c78f11b5baea0e8a167a714ad03f1
MingFei2001 <mingfei2001@gmail.com>
2024-07-18 03:33:34 +0800

    fixed which-key bug
    the keybinding format was different after the update therefore this
    commit is to remedy that issue

---
+++ editor/nvim/nvim/lua/custom/plugins/markdown-preview.lua
@@ -0,0 +1,10 @@
+-- Markdown Preview
+
+return {
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  ft = { 'markdown' },
+  build = function()
+    vim.fn['mkdp#util#install']()
+  end,
+}
