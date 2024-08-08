098321a447e299e011fbf01822ee57812b96e043
MingFei2001 <mingfei2001@gmail.com>
2024-08-09 01:44:46 +0800

    added markview.nvim

---
+++ editor/nvim/nvim/lua/custom/plugins/lazygit.lua
@@ -0,0 +1,23 @@
+return {
+  'kdheepak/lazygit.nvim',
+  cmd = {
+    'LazyGit',
+    'LazyGitConfig',
+    'LazyGitCurrentFile',
+    'LazyGitFilter',
+    'LazyGitFilterCurrentFile',
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+  },
+  -- setting the keybinding for LazyGit with 'keys' is recommended in
+  -- order to load the plugin when the command is run for the first time
+  keys = {
+    { '<leader>g', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+  },
+  config = function()
+    require('telescope').load_extension 'lazygit'
+  end,
+}
