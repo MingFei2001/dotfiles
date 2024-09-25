08e4558fe87aa1b402852235e898290c4394dc5d
MingFei2001 <mingfei2001@gmail.com>
2024-09-25 21:43:38 +0800

    install kanagawa but do not use & uninstall

--- editor/nvim/init.lua
+++ editor/nvim/init.lua
@@ -643,12 +643,28 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'rebelot/kanagawa.nvim',
+  },
+  -- priority = 1000, -- make sure to load this before all the other start plugins
+  -- init = function()
+  --   -- Load the colorscheme here.
+  --   -- Like many other themes, this one has different styles, and you could load
+  --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --   vim.cmd.colorscheme 'kanagawa'
+  -- end,
+
+  {
+    'neanias/everforest-nvim',
+    -- version = false,
+    -- lazy = false,
     priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
     init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'kanagawa'
+      vim.cmd.colorscheme 'everforest'
+    end,
+    config = function()
+      require('everforest').setup {
+        background = 'hard',
+      }
     end,
   },
 
